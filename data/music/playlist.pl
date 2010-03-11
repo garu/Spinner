83e6beaf181ee3c3b88c4d36dd365479ca3ab41a
Kartik Thakore <thakore.kartik@gmail.com>
2010-03-11 07:46:38 -0500

    Trying to add spiffy bg

--- data/music/playlist.pl
+++ data/music/playlist.pl
@@ -1,25 +1,28 @@
 use strict;
 use warnings;
 use File::Spec;
-#use threads;
-#use threads::shared;
+use threads;
+use threads::shared;
 use SDL;
 use SDL::Mixer;
 use SDL::Mixer::Music;
-#use SDL::Video;
+use SDL::Mixer::Effects;
+use SDL::Video;
 use SDL::Event;
 use SDL::Events;
 use Carp;
 
 
+my $background :shared = 0;
+
 # Initing video
 #Die here if we cannot make video init
 croak 'Cannot init ' . SDL::get_error()
-if ( SDL::init( SDL_INIT_AUDIO ) == -1 );
+if ( SDL::init( SDL_INIT_AUDIO | SDL_INIT_VIDEO ) == -1 );
 
-#my $app = SDL::Video::set_video_mode( 800, 600, 32,
-#	                    SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_HWACCEL );
-		     
+my $app = SDL::Video::set_video_mode( 800, 600, 32,
+	SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_HWACCEL );
+
 
 SDL::Mixer::open_audio( 44100, AUDIO_S16, 2, 4096 );
 
@@ -55,40 +58,57 @@ SDL::Mixer::Music::hook_music_finished(	$callback );
 
 @songs = sort { int( rand(2) - rand(2)) } @songs;
 
+my $event = SDL::Event->new();
+
+sub spiffy
+{
+	my $channel  = shift;
+	my $samples  = shift;
+	my $position = shift;
+	my @stream   = @_;
+	print join("  | ", @stream);
+	print "\n";
+}
+
+sub spiffydone
+{
+
+
+}
+
+
 foreach (@songs)
 {
 	warn 'Playing '.$_  ;
 	my $song = SDL::Mixer::Music::load_MUS ( $_);
-	SDL::Mixer::Music::play_music( $song, 0 );
+	my $channel = SDL::Mixer::Music::play_music( $song, 0 );
+	my $effect_id = SDL::Mixer::Effects::register($channel, "main::spiffy", "main::spiffydone", 0);
+
 	$music_is_playing = 1;
+
 
 	while ( $music_is_playing)
 	{
-		#chop (my $quit = <STDIN>);
-		#exit if $quit =~ /q/;
-		#if ( $quit =~ /n/ )
-		#{
-		#SDL::Mixer::Music::halt_music(); 		
-		# warn $music_is_playing--;
-		#}
+		while ( SDL::Events::poll_event($event) )
+		{
 
+			if ( $event->type == SDL_QUIT ) {
+				exit;
+			}
+			elsif ( $event->type == SDL_KEYDOWN ) {
+
+				if ( $event->key_sym == SDLK_DOWN ) {
+					&$callback();
+				}
+			}
+
+		}
 		SDL::delay(100);
 	}
+
+	SDL::Mixer::Effects::unregister($channel, $effect_id);
 
 }
 
 SDL::Mixer::Music::hook_music_finished();
-#		while ( SDL::Events::poll_event($event) )
-#		{
-#
-#	if ( $event->type == SDL_QUIT ) {
-#		exit;
-#	}
-#	elsif ( $event->type == SDL_KEYDOWN ) {
-#
-#		if ( $event->key_sym == SDLK_DOWN ) {
-#		}
-#	}
-
-#}
 
