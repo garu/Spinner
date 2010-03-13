21ea86625b7d5e443153a7aee4f689086302978c
Breno G. de Oliveira <garu@cpan.org>
2010-03-13 05:22:35 -0300

    'enter high score' implemented!

--- spinner.pl
+++ spinner.pl
@@ -136,6 +136,78 @@ sub menu {
     }
 }
 
+sub enter_highscore {
+    # check if player made a high score
+    my $high_score = Spinner::load_data_file('data/highscore.dat');
+    my $rank = 0;
+    while (exists $high_score->[$rank]) {
+        last if $score > $high_score->[$rank]->{score};
+        $rank++;
+    }
+    return unless $rank < 10;
+
+    # remove last entry
+    pop @{$high_score};
+
+    my $font = SDL::TTF::open_font('data/metro.ttf', 18)
+        or Carp::croak 'Error opening font: ' . SDL::get_error;
+    my $color = SDL::Color->new(100,255,100);
+
+    my $show = 1;
+    my $name = '';
+    my $event = SDL::Event->new;
+    while ($show) {
+        while ( SDL::Events::poll_event($event) ) {
+            if ($event->type == SDL_KEYDOWN) {
+                my $key = $event->key_sym;
+                if (length $name == 13
+                   or $key == SDLK_RETURN or $key == SDLK_KP_ENTER
+                ) {
+                    $show = 0;
+                }
+                elsif ( ($key >= SDLK_0 and $key <= SDLK_9)
+                     or ($key >= SDLK_a and $key <= SDLK_z)
+                ) {
+                    $name .= chr($key);
+                }
+            }
+        }
+        SDL::Video::fill_rect( $app, SDL::Rect->new(100,100, 520,170),
+                SDL::Video::map_RGB( $app->format, 0, 0, 0 )
+        );
+        SDL::Video::fill_rect( $app, SDL::Rect->new(110,110, 500,150),
+                SDL::Video::map_RGB( $app->format, 0, 50, 0 )
+        );
+
+        # blit the window title
+        my $title = SDL::TTF::render_text_blended(
+                $font, "New High Score! $score", $color
+                ) or Carp::croak 'TTF render error: ' . SDL::get_error;
+        SDL::Video::blit_surface(
+                $title,
+                SDL::Rect->new(0,0,$title->w, $title->h),
+                $app,
+                SDL::Rect->new(200, 130, $app->w, $app->h),
+        );
+
+        # blit the user name
+        my $player = SDL::TTF::render_text_blended($font, $name . '_', $color)
+            or Carp::croak 'TTF render error: ' . SDL::get_error;
+        SDL::Video::blit_surface(
+                $player, SDL::Rect->new(0,0,$player->w, $player->h),
+                $app, SDL::Rect->new($app->w / 2 - 10 * length($name) - 50, 190, $app->w, $app->h),
+        );
+
+        SDL::Video::flip($app);
+    }
+
+    # add the new score to the table
+    splice @{$high_score}, $rank, 0, { name => $name, score => $score };
+    Spinner::write_data_file('data/highscore.dat', $high_score);
+
+    return high_scores();
+}
+
 
 sub high_scores {
     my $show = 1;
@@ -219,6 +291,7 @@ sub game {
                 );
                 SDL::Video::flip($app);
                 SDL::delay(1000);
+                enter_highscore();
                 last;
             }
         }
