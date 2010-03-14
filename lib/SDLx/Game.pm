ef0fe741ec07525d657c82f9a654cb7fac84dbe8
Kartik Thakore <thakore.kartik@gmail.com>
2010-03-14 15:51:45 -0400

    A simple module to handle the stupid game loop so I don't have to keep remaking this from scratch each time I make a game AAAHAHAHAHAHAHAH!

---
+++ lib/SDLx/Game.pm
@@ -0,0 +1,25 @@
+package SDLx::Game;
+
+
+1; #not 42 man!
+
+=pod 
+
+=head1 NAME
+
+SDLx::Game - Handles the game loop 
+
+=head2 Description
+
+Using http://www.lazyfoo.net/SDL_tutorials/lesson32/index.php as our base
+
+=head1 METHODS
+
+
+=head2 run
+
+=head2 move_callback 
+
+=head2 render_callback
+
+=cut
