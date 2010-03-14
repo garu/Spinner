206d6c954b46485670bc9d261f474a2d588d8a47
Kartik Thakore <thakore.kartik@gmail.com>
2010-03-14 15:59:44 -0400

    Laying it out

--- lib/SDLx/Game.pm
+++ lib/SDLx/Game.pm
@@ -1,6 +1,37 @@
 package SDLx::Game;
 
 
+sub new
+{
+
+
+}
+
+sub run
+{
+
+}
+
+
+sub _event
+{
+
+}
+
+
+sub _move
+{
+
+}
+
+sub _show
+{
+
+}
+
+
+
+
 1; #not 42 man!
 
 =pod 
@@ -21,5 +52,7 @@ Using http://www.lazyfoo.net/SDL_tutorials/lesson32/index.php as our base
 =head2 move_callback 
 
 =head2 render_callback
+
+=head2 current_fps
 
 =cut
